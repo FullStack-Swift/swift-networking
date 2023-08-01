481eac1d78c7c8d8111788b4fdccc27ceabe5138
NguyenPhongVN <nguyenphong.mobile.engineer@gmail.com>
2023-08-01 10:49:09 +0700

    fix some issue
    Signed-off-by: NguyenPhongVN <nguyenphong.mobile.engineer@gmail.com>

--- Examples/Examples/ExamplesApp.swift
+++ Examples/Examples/ExamplesApp.swift
@@ -1,10 +1,31 @@
 import SwiftUI
+import MCombineRequest
+import Transform
 
 @main
 struct ExamplesApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      Text("RT")
+        .task {
+          Task {
+            let data = try await MRequest {
+              RUrl(urlString: "http://127.0.0.1:8080")
+                .withPath("todos")
+              Rbody(Todo(id: UUID(), text: "_text", isCompleted: false).toData())
+              RMethod(.post)
+              REncoding(JSONEncoding.default)
+            }
+              .printCURLRequest()
+              .data
+          }
+        }
     }
   }
+}
+
+private struct Todo: Codable, Hashable, Identifiable {
+  var id: UUID
+  var text: String
+  var isCompleted: Bool
 }
--- Sources/BuildRequest/Core/RequestProtocol.swift
+++ Sources/BuildRequest/Core/RequestProtocol.swift
@@ -18,10 +18,7 @@ extension RequestProtocol {
     var items: [RequestProtocol] = []
     items.append(contentsOf: requests.filter({$0 is RUrl}))
     items.append(contentsOf: requests.filter({$0 is RPath}))
-    items.append(contentsOf: requests.filter({$0 is Rbody}))
-    items.append(contentsOf: requests.filter({$0 is RMethod}))
-    items.append(contentsOf: requests.filter({$0 is RHeaders}))
-    items.append(contentsOf: requests.filter({$0 is RQueryItem || $0 is RQueryItems}))
+    items.append(contentsOf: requests.filter({!($0 is RUrl || $0 is REncoding || $0 is RPath)}))
     items.append(contentsOf: requests.filter({$0 is REncoding}))
     /// build
     items.forEach {
